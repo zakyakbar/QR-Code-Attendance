f28d31bf12cac0ccdc42046c415b891c3086b9b1
Zaky akbar <60866227+zakyakbar@users.noreply.github.com>
2021-06-13 14:18:17 +0700

    Add files via upload

---
+++ apiabsen/jarkom.php
@@ -0,0 +1,40 @@
+<?php
+    
+    $connect= mysqli_connect("localhost", "root", "", "absensi");
+    
+    $name = $_POST["name"];
+    $nim = $_POST["nim"];
+
+     function registerUser() {
+        global $connect, $name, $nim;
+        $statement = mysqli_prepare($connect, "INSERT INTO absen_jarkom (name, nim) VALUES (?, ?)");
+    	mysqli_stmt_bind_param($statement, "si", $name, $nim);
+        mysqli_stmt_execute($statement);
+        mysqli_stmt_close($statement);     
+    }
+
+    function usernameAvailable() {
+        global $connect, $nim;
+        $statement = mysqli_prepare($connect, "SELECT * FROM krs_jarkom WHERE nim = ?"); 
+        mysqli_stmt_bind_param($statement, "s", $nim);
+        mysqli_stmt_execute($statement);
+        mysqli_stmt_store_result($statement);
+        $count = mysqli_stmt_num_rows($statement);
+        mysqli_stmt_close($statement); 
+        if ($count == 1){
+            return true; 
+        }else {
+            return false; 
+        }
+    }
+
+    $response = array();
+    $response["success"] = false;  
+
+    if (usernameAvailable()){
+        registerUser();
+        $response["success"] = true;  
+    }
+    
+    echo json_encode($response);
+?>
